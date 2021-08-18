5bc1e7a387895984c98bbfe571d1ca8ef481feca
Joseph <21873267@sun.ac.za>
2021-08-18 20:17:18 +0200

    Added post delete and update functions. Not sure about how code is structured and recieving the id needed to delete/edit a post

--- src/flask_blog/mutations/posts.py
+++ src/flask_blog/mutations/posts.py
@@ -31,3 +31,59 @@ class CreatePost(relay.ClientIDMutation):
         db.session.commit()
 
         return CreatePostSuccess(post=new_post)
+
+
+class UpdatePostInput:
+    id = graphene.String(required=True)
+    title = graphene.String(required=True)
+    content = graphene.String(required=True)
+
+
+class UpdatePostSuccess(graphene.ObjectType):
+    post = graphene.Field(types.PostNode, required=True)
+
+
+class UpdatePostOutput(graphene.Union):
+    class Meta:
+        types = (UpdatePostSuccess,)
+
+
+class UpdatePost(relay.ClientIDMutation):
+    Input = UpdatePostInput
+    Output = UpdatePostOutput
+
+    @classmethod
+    def mutate_and_get_payload(cls, root, info, **input):
+        new_post = models.Post.query.get(int(input["id"]))
+        new_post.title = input["title"]
+        new_post.content = input["content"]
+        db.session.commit()
+
+        return UpdatePostSuccess(post=new_post)
+
+
+class DeletePostInput:
+    id = graphene.String(required=True)
+
+
+class DeletePostSuccess(graphene.ObjectType):
+    post = graphene.Field(types.PostNode, required=True)
+
+
+
+class DeletePostOutput(graphene.Union):
+    class Meta:
+        types = (DeletePostSuccess,)
+
+
+class DeletePost(relay.ClientIDMutation):
+    Input = DeletePostInput
+    Output = DeletePostOutput
+
+    @classmethod
+    def mutate_and_get_payload(cls, root, info, **input):
+        new_post = models.Post.query.get(int(input["id"]))
+        db.session.delete(new_post)
+        db.session.commit()
+
+        return DeletePostSuccess(post=new_post)
