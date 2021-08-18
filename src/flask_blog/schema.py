65497c1e5e1c7f0577fc845659f36f4c5cc9f5d1
Joseph <21873267@sun.ac.za>
2021-08-18 20:20:35 +0200

    Added code to query tags, though probably not needed

--- src/flask_blog/schema.py
+++ src/flask_blog/schema.py
@@ -1,24 +1,40 @@
 import graphene
-from graphene import relay
+from graphene import relay, List
 from graphene_sqlalchemy import SQLAlchemyConnectionField
 
 from . import models, mutations
-from .types import PostConnection
-
+from .types import PostConnection, TagConnection
 
 class Query(graphene.ObjectType):
     node = relay.Node.Field()
     posts = SQLAlchemyConnectionField(PostConnection)
+    tags = SQLAlchemyConnectionField(TagConnection)
+
+    def resolve_post_tags(self, info, *args, **kwargs):
+        post_tags = List(models.Post.post_tags)
+
+        query = SQLAlchemyConnectionField.get_query(
+            post_tags, info, *args, **kwargs
+        )
+        
+        return query.all()
 
     def resolve_posts(self, info, *args, **kwargs):
         query = SQLAlchemyConnectionField.get_query(
             models.Post, info, *args, **kwargs
         )
         return query.all()
-
-
+    
+    def resolve_tags(self, info, *args, **kwargs):
+        query = SQLAlchemyConnectionField.get_query(
+            models.Tag, info, *args, **kwargs
+        )
+        return query.all()
+    
 class Mutation(graphene.ObjectType):
     create_post = mutations.CreatePost.Field()
+    update_post = mutations.UpdatePost.Field()
+    delete_post = mutations.DeletePost.Field()
 
 
 schema = graphene.Schema(query=Query, mutation=Mutation)
