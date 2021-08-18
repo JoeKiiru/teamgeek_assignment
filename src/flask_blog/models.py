04d2bd6e19eb31790f0dfe56e22bff4a59969fa9
Joseph <21873267@sun.ac.za>
2021-08-18 08:23:47 +0200

    Added Tag model in order to store and query tags added to a post

--- src/flask_blog/models.py
+++ src/flask_blog/models.py
@@ -9,8 +9,19 @@ class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String, nullable=False)
     content = db.Column(db.Text, nullable=False)
+    tag = db.relationship("Tag", primaryjoin='Post.id == foreign(Tag.id)',)
     created_at = db.Column(db.DateTime, default=datetime.now, nullable=False)
     updated_at = db.Column(db.DateTime, onupdate=datetime.now)
 
     def __repr__(self):
         return "<Post id={}, title='{}'>".format(self.id, self.title)
+
+
+class Tag(db.Model):
+    __tablename__ = "tags"
+
+    id = db.Column(db.Integer, primary_key=True)
+    tag = db.Column(db.String, nullable=True)
+
+    def __repr__(self):
+        return "<Tag id={}, tag='{}'>".format(self.id, self.tag)
