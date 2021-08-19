7d9211ef0ae60fd85e896c6bd861b4e02ba41e61
Joseph <21873267@sun.ac.za>
2021-08-19 18:42:36 +0200

    Finished filtering by date

--- src/flask_blog/schema.py
+++ src/flask_blog/schema.py
@@ -1,13 +1,20 @@
 import graphene
 from graphene import relay, List
 from graphene_sqlalchemy import SQLAlchemyConnectionField
+from datetime import datetime, timedelta
+
+from flask_blog.views.posts import filter_by_creation
 
 from . import models, mutations
 from .types import PostConnection, TagConnection
 
 class Query(graphene.ObjectType):
     node = relay.Node.Field()
-    posts = SQLAlchemyConnectionField(PostConnection, tag=graphene.String())
+    posts = SQLAlchemyConnectionField(
+        PostConnection, 
+        tag=graphene.String(),
+        createdAt=graphene.String()
+    )
     tags = SQLAlchemyConnectionField(TagConnection)
     
     def resolve_post_tags(self, info, *args, **kwargs):
@@ -26,7 +33,28 @@ class Query(graphene.ObjectType):
         if "tag" in kwargs:
             tag_name = kwargs["tag"]
             query = query.filter(models.Post.post_tags.any(tag=tag_name))
+        elif "createdAt" in kwargs:
+            filter_type = kwargs["createdAt"]
+            if filter_type == "newest":
+                query = models.Post.query.order_by(models.Post.created_at.desc())
+            elif filter_type == "oldest":
+                query = models.Post.query.order_by(models.Post.created_at.asc())
+            else:
+                pastTime = 0
+                if filter_type == "lasthour":
+                    pastTime = datetime.now() - timedelta(hours=1)
+                elif filter_type == "lastday":
+                    pastTime = datetime.now() - timedelta(days=1)
+                elif filter_type == "last7days":
+                    pastTime = datetime.now() - timedelta(days=7)
+                elif filter_type == "lastmonth":
+                    pastTime = datetime.now() - timedelta(days=30)
+                elif filter_type == "lastyear":
+                    pastTime = datetime.now() - timedelta(days=365)
         
+                query = models.Post.query.filter(models.Post.created_at >= pastTime)
+                query = query.order_by(models.Post.created_at.desc())
+
         return query.all()
     
     def resolve_tags(self, info, *args, **kwargs):
