c925defc9d81577808bbb4faffc556e45549ae4d
Joseph <21873267@sun.ac.za>
2021-08-19 17:44:33 +0200

    Added filtering by tag

--- src/flask_blog/schema.py
+++ src/flask_blog/schema.py
@@ -7,22 +7,25 @@ from .types import PostConnection, TagConnection
 
 class Query(graphene.ObjectType):
     node = relay.Node.Field()
-    posts = SQLAlchemyConnectionField(PostConnection)
+    posts = SQLAlchemyConnectionField(PostConnection, tag=graphene.String())
     tags = SQLAlchemyConnectionField(TagConnection)
-
+    
     def resolve_post_tags(self, info, *args, **kwargs):
         post_tags = List(models.Post.post_tags)
-
+        
         query = SQLAlchemyConnectionField.get_query(
             post_tags, info, *args, **kwargs
         )
-        
         return query.all()
 
-    def resolve_posts(self, info, *args, **kwargs):
+    def resolve_posts(self, info, *args, **kwargs,):
         query = SQLAlchemyConnectionField.get_query(
             models.Post, info, *args, **kwargs
         )
+        tag_name = kwargs["tag"]
+        if tag_name:
+            query = query.filter(models.Post.post_tags.any(tag=tag_name))
+        
         return query.all()
     
     def resolve_tags(self, info, *args, **kwargs):
